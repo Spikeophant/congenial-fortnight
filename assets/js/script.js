29c6b1fef13321584113330b05cef517f68ddac4
Dustin Davisson <dustin.davisson@gmail.com>
2022-07-28 21:57:02 -0700

    Fonts, basic structure, initial js.

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+// Quiz!
+
+//array of question/anwer pairs.
+// get real question later.
+questions = {
+    'Question 1?': 'answer',
+    'Question 2?': 'answer'
+};
+
+//list of all answers, we'll use these randomly to fill in the other questions for fun.
+wrongAnswers = ['otherAnswer', 'otherAnswer333'];
+
+//get local storage for high scores.
+scores = localStorage.getItem('scores');
+var displayScores = function (scores) {
+    console.log(scores);
+
+};
+
+var askQuestion = function (questions, wrongAnswers) {
+
+    return true;
+};
+
+askQuestion();
